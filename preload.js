d123e2069ab72e656a7abea0c4c877d5537f3ce7
silvajjuniorpro-svg <silva.jjuniorpro@gmail.com>
2025-12-18 12:04:33 -0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,10 +1,8 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
-  listLeads: () => ipcRenderer.invoke('leads:list'),
-  createLead: (lead) => ipcRenderer.invoke('leads:create', lead),
-  updateLead: (lead) => ipcRenderer.invoke('leads:update', lead),
-  deleteLead: (id) => ipcRenderer.invoke('leads:delete', id),
-  exportCsv: () => ipcRenderer.invoke('leads:exportCsv'),
-  importCsv: () => ipcRenderer.invoke('leads:importCsv'),
+  list: () => ipcRenderer.invoke('leads:list'),
+  create: (lead) => ipcRenderer.invoke('leads:create', lead),
+  delete: (id) => ipcRenderer.invoke('leads:delete', id),
+  exportCsv: () => ipcRenderer.invoke('leads:exportCsv')
 });
